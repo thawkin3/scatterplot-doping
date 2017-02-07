e60d98a48e2297713faefc006155ef77ed27feec
Tyler Hawkins <thawkin3@byu.net>
2017-02-06 23:44:52 -0700

    finished up the scatterplot. looking good

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
 
+	// URL FOR READING IN THE DATA
 	var url = "javascripts/cyclist-data.json";
+	
+	// HELPER FORMULAS FOR FORMATTING THE TIME IN THE X-AXIS
 	var formatTime = d3.time.format("%M:%S");
 	var formatMinutes = function(d) {
-	    var time = new Date(2012, 0, 1, 0, d)
-	    time.setSeconds(time.getSeconds() + d);
+	    var time = new Date(2012, 0, 1, 0, Math.floor(d/60), d%60);
 	    return formatTime(time);
 	};
 
@@ -23,7 +25,7 @@ $(document).ready(function() {
 		// SET SOME STYLING VARIABLES FOR THE CHART
 		var margin = {
 			top: 5,
-			right: 10,
+			right: 20,
 			bottom: 50,
 			left: 75
 		};
@@ -33,13 +35,14 @@ $(document).ready(function() {
 
 		// X SCALE
 		// TAKE THE MIN AND THE MAX FINISH TIMES AND FIT IT TO THE WIDTH WE SET EARLIER
-		// TIME.SCALE IS A MODIFCATION TO SCALE.LINEAR TO WORK WELL WITH DATES
+		// ALSO ADD A FEW SECONDS OF PADDING ON EITHER SIDE TO GIVE US SOME ROOM
+		// REVERSE THE RANGE SO THAT THE SMALLER VALUES ARE ON THE RIGHT
 		var x = d3.scale.linear()
-			.domain([d3.min(data, function(d) {return d.Seconds;}), d3.max(data, function(d) {return d.Seconds;})])
-			.range([0, width]);
+			.domain([d3.min(data, function(d) {return d.Seconds - 20;}), d3.max(data, function(d) {return d.Seconds + 5;})])
+			.range([width, 0]);
 
 		// Y SCALE
-		// SET THE DOMAIN TO GO FROM 0 TO THE LARGEST VALUE OF OUR DATA SET
+		// SET THE DOMAIN TO GO FROM 1 TO 36 SINCE WE HAVE 35 DATA POINTS AND WANT A LITTLE EXTRA SPACE AT THE BOTTOM
 		// AND FIT IT TO OUR HEIGHT WE SET EARLIER
 		// BUT IT'S FLIPPED BECAUSE WE GO FROM TOP-LEFT TO BOTTOM-RIGHT WITH SVG
 		var y = d3.scale.linear()
@@ -47,15 +50,15 @@ $(document).ready(function() {
 			.range([0, height]);
 
 		// X-AXIS
-		// PUT THE X-AXIS ON THE BOTTOM AND FORMAT THE TICKS TO BE YEARS AT 5-YEAR INTERVALS
+		// PUT THE X-AXIS ON THE BOTTOM AND FORMAT THE TICKS TO BE MINUTES AND SECONDS
 		var xAxis = d3.svg.axis()
 			.scale(x)
 			.orient("bottom")
-			.tickValues([35,36,37,38,39,40])
+			.ticks(10,"")
 			.tickFormat(formatMinutes);
 
 		// Y-AXIS
-		// PUT THE Y-AXIS ON THE LEFT AND FORMAT THERE TO BE 10 TICKS AND TO HAVE A $ PREFIX AND COMMAS
+		// PUT THE Y-AXIS ON THE LEFT AND FORMAT THERE TO BE 7 TICKS FOR THE RANKINGS
 		var yAxis = d3.svg.axis()
 			.scale(y)
 			.orient("left")
@@ -101,54 +104,85 @@ $(document).ready(function() {
 
 		// CREATE THE BARS THAT MAKE UP OUR BAR CHART
 		// GO THROUGH EACH DATA POINT
-		// CREATE A RECTANGLE FOR IT
-		// SET THE X COORDINATE FOR IT USING THE DATE IN MILLISECONDS
-		// SET THE Y COORDINATE FOR IT USING THE GDP VALUE
-		// SET THE HEIGHT OF THE BAR BY FINDING THE DISTANCE FROM THE TOP OF THE BAR TO THE 0 POINT
-		// SET THE WIDTH OF THE BAR USING THE EQUALLY DIVIDED WIDTHS WE CALCULATED EARLIER
-		// FINALLY, SET THE MOUSEOVER/MOUSEOUT EFFECTS FOR THE TOOLTIP, SETTING THE TOOLTIP CONTENT AND POSITION BASED ON THE BAR HOVERED OVER
-		chart.selectAll(".bar")
+		// CREATE A CIRCLE FOR IT
+		// SET THE CX COORDINATE FOR IT USING THE TIME IN SECONDS
+		// SET THE CY COORDINATE FOR IT USING THE RANKING
+		// SET ALL THE RADII TO BE THE SAME SIZE
+		// SET THE COLOR OF THE CIRCLE BASED ON IF THERE WAS A DOPING ALLEGATION OR NOT
+		// FINALLY, SET THE MOUSEOVER/MOUSEOUT EFFECTS FOR THE TOOLTIP, CHANGING THE CONTENT BASED ON THE SELECTED DATA POINT
+		chart.selectAll(".circle")
 			.data(data)
-			.enter().append("rect")
-			.attr("class", "bar")
-			.attr("x", function(d) {
-				return x(new Date(d[0]));
+			.enter()
+			.append("circle")
+			.attr("class", "circle")
+			.attr("cx", function(d) {
+				return x(d.Seconds);
 			})
-			.attr("y", function(d) {
-				return y(d[1]);
+			.attr("cy", function(d) {
+				return y(d.Place);
 			})
-			.attr("height", function(d) {
-				return height - y(d[1]);
+			.attr("r", 5)
+			.attr("fill", function(d) {
+				if (d.Doping == "") {
+					return "#35de1e";
+				} else {
+					return "#de1e1e";
+				}
 			})
-			.attr("width", barWidth)
 			.on("mouseover", function(d) {
-				var rect = d3.select(this);
-				rect.attr("class", "mouseover");
-				var currentDateTime = new Date(d[0]);
-				var year = currentDateTime.getUTCFullYear();
-				var month = currentDateTime.getUTCMonth();
-				var dollars = d[1];
+				var circleDataPoint = d3.select(this);
+				circleDataPoint.attr("class", "mouseover");
 				tooltip.transition()
 					.duration(200)
 					.style("opacity", 0.9);
-				tooltip.html("<span class='amount'>" + formatCurrency(dollars) + " Billion </span><br><span class='year'>" + year + " - " + months[month] + "</span>")
-					.style("top", (d3.event.pageY - 50) + "px");
-				if (d3.event.offsetX - 80 < width/2) {
-					tooltip.style("left", (d3.event.pageX + 5) + "px")
-							.style("width", "126px");
-				} else {
-					if (dollars < 10000) {
-						tooltip.style("left", (d3.event.pageX - 145) + "px")
-							.style("width", "126px");
-					} else {
-						tooltip.style("left", (d3.event.pageX - 155) + "px")
-							.style("width", "136px");
-					}
+				if (d.Doping == "") {
+					d.Doping = "No doping allegations";
 				}
+				tooltip.html("<p><strong>" + d.Name + " (" + d.Nationality + ")</strong></p>" + 
+					"<p>Year: " + d.Year + ", Time: " + d.Time + "</p>" +
+					"<p>&nbsp;</p>" + 
+					"<p>" + d.Doping + "</p>")
 			})
 			.on("mouseout", function() {
-				var rect = d3.select(this);
-				rect.attr("class", "mouseoff");
+				var circleDataPoint = d3.select(this);
+				circleDataPoint.attr("class", "mouseoff");
+				tooltip.transition()
+					.duration(500)
+					.style("opacity", 0);
+			});
+
+		// BIKER NAME LABELS
+		// ADD THESE NEXT TO EACH DATA POINT
+		chart.selectAll(".bikerLabel")
+			.data(data)
+			.enter()
+			.append("text")
+			.attr("class", "bikerLabel")
+			.text(function(d) {
+				return d.Name;
+			})
+			.attr("x", function(d) {
+				return x(d.Seconds);
+			})
+			.attr("y", function(d) {
+				return y(d.Place);
+			})
+			.attr("dx", "10px")
+			.attr("dy", "4px")
+			.attr("text-anchor", "start")
+			.on("mouseover", function(d) {
+				tooltip.transition()
+					.duration(200)
+					.style("opacity", 0.9);
+				if (d.Doping == "") {
+					d.Doping = "No doping allegations";
+				}
+				tooltip.html("<p><strong>" + d.Name + " (" + d.Nationality + ")</strong></p>" + 
+					"<p>Year: " + d.Year + ", Time: " + d.Time + "</p>" +
+					"<p>&nbsp;</p>" + 
+					"<p>" + d.Doping + "</p>")
+			})
+			.on("mouseout", function() {
 				tooltip.transition()
 					.duration(500)
 					.style("opacity", 0);
